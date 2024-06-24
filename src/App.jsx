261393874595ae8bb222fb43394dd29754472c89
Amey Kurade <ameykurade60@gmail.com>
2024-06-24 23:43:44 +0530

    Vercel Analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./Components/Hero/Hero";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
     <div>
       <Hero/>
+      <Analytics />
     </div>
   );
 }
