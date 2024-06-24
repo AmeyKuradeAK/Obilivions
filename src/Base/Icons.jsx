44e2757556ddde74ecd20f0e58e6d11ba59f4154
Amey Kurade <ameykurade60@gmail.com>
2024-06-24 22:19:19 +0530

    Responsive to Mobile device avaible

--- src/Base/Icons.jsx
+++ src/Base/Icons.jsx
@@ -4,19 +4,21 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Icons = () => {
   return (
-    <div className="flex flex-col justify-center ite h-full px-8">
-      <a href="#" className="mb-4 text-gray-600 hover:text-gray-800">
-        <FaInstagram class="h-6 w-6 fill-current" />
-      </a>
-      <a href="#" className="mb-4 text-gray-600 hover:text-gray-800">
-        <FaFacebook class="h-6 w-6 fill-current" />
-      </a>
-      <a href="#" className="text-gray-600 hover:text-gray-800">
-        <FaSquareXTwitter class="h-6 w-6 fill-current" />
-      </a>
-      <a href="#" className="text-gray-600 hover:text-gray-800">
-        <FaLinkedin class="h-6 w-6 fill-current" />
-      </a>
+    <div className="sm:max-2xl:justify-center sm:max-2xl:items-center sm:max-2xl:h-full">
+      <div className="flex flex-col justify-center ite h-full px-8">
+        <a href="#" className="mb-4 text-gray-600 hover:text-gray-800">
+          <FaInstagram class="h-6 w-6 fill-current" />
+        </a>
+        <a href="#" className="mb-4 text-gray-600 hover:text-gray-800">
+          <FaFacebook class="h-6 w-6 fill-current" />
+        </a>
+        <a href="#" className="text-gray-600 hover:text-gray-800">
+          <FaSquareXTwitter class="h-6 w-6 fill-current" />
+        </a>
+        <a href="#" className="text-gray-600 hover:text-gray-800">
+          <FaLinkedin class="h-6 w-6 fill-current" />
+        </a>
+      </div>
     </div>
   );
 };
--- src/Components/Hero/Navdemo.jsx
+++ src/Components/Hero/Navdemo.jsx
@@ -4,19 +4,23 @@ import Icons from "../../Base/Icons";
 
 const Navdemo = () => {
   return (
-    <nav className="sm:max-2xl:flex sm:max-2xl:items-center sm:max-2xl:justify-between sm:max-2xl:h-screen bg-navbar-3">
+    <nav className="sm:max-2xl:flex sm:max-2xl:items-center sm:max-2xl:justify-between sm:max-2xl:h-screen bg-navbar-3 drop-shadow-2xl">
       <div className="sm:max-2xl:flex sm:max-2xl:justify-center sm:max-2xl:items-center sm:max-2xl:h-full sm:max-2xl:px-8">
-        <div className="sm:max-xl2:text-left font-Main sm:max-2xl:text-4xl">
+        <div className="sm:max-xl2:text-left font-Main sm:max-2xl:text-4xl mb-10">
           <p className="text-gray-800 text-center">India's</p>
           <p className="text-gray-800 text-center">Upcoming</p>
           <p className="text-gray-800 text-center">eSports Team</p>
         </div>
       </div>
-      <div className="sm:max-2xl:flex sm:max-2xl:justify-center sm:max-2xl:items-center sm:max-2xl:h-full">
-        <img src={Obi} alt="Company Logo" className="sm:max-2xl:h-96 sm:max-2xl:w-96 sm:max-2xl:rounded-full" />
+      <div className="sm:max-2xl:flex sm:max-2xl:justify-center sm:max-2xl:items-center sm:max-2xl:h-full mb-10">
+        <img
+          src={Obi}
+          alt="Company Logo"
+          className="sm:max-2xl:h-96 sm:max-2xl:w-96 sm:max-2xl:rounded-full"
+        />
       </div>
-      <div className="sm:max-2xl:flex sm:max-2xl:flex-col sm:max-2xl:justify-center sm:max-2xl:items-center sm:max-2xl:h-full sm:max-2xl:px-8">
-        <Icons />
+      <div className="flex flex-col justify-center items-center">
+          <Icons />
       </div>
     </nav>
   );
