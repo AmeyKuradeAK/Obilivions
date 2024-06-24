4d32d939ed9ad8f6d9471e26e6ee3effd2e984f9
Amey Kurade <ameykurade60@gmail.com>
2024-06-24 23:29:31 +0530

    Totally optimized for mobile

--- src/Base/Icons.jsx
+++ src/Base/Icons.jsx
@@ -4,7 +4,7 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Icons = () => {
   return (
-    <div className="sm:max-2xl:justify-center sm:max-2xl:items-center sm:max-2xl:h-full">
+    <div className="sm:max-2xl:justify-center sm:max-2xl:items-center sm:max-2xl:h-full sm:max-2xl:mr-10">
       <div className="flex flex-col justify-center ite h-full px-8">
         <a href="#" className="mb-4 text-gray-600 hover:text-gray-800">
           <FaInstagram class="h-6 w-6 fill-current" />
--- src/Components/Hero/Navdemo.jsx
+++ src/Components/Hero/Navdemo.jsx
@@ -6,7 +6,7 @@ const Navdemo = () => {
   return (
     <nav className="sm:max-2xl:flex sm:max-2xl:items-center sm:max-2xl:justify-between sm:max-2xl:h-screen bg-navbar-3 drop-shadow-2xl">
       <div className="sm:max-2xl:flex sm:max-2xl:justify-center sm:max-2xl:items-center sm:max-2xl:h-full sm:max-2xl:px-8">
-        <div className="sm:max-xl2:text-left font-Main sm:max-2xl:text-4xl mb-10">
+        <div className="sm:max-xl2:text-left font-Main sm:max-2xl:text-5xl text-5xl mb-10">
           <p className="text-gray-800 text-center">India's</p>
           <p className="text-gray-800 text-center">Upcoming</p>
           <p className="text-gray-800 text-center">eSports Team</p>
@@ -16,7 +16,7 @@ const Navdemo = () => {
         <img
           src={Obi}
           alt="Company Logo"
-          className="sm:max-2xl:h-96 sm:max-2xl:w-96 sm:max-2xl:rounded-full"
+          className="sm:max-2xl:h-max h-max sm:max-2xl:w-max w-max sm:max-2xl:rounded-full"
         />
       </div>
       <div className="flex flex-col justify-center items-center">
